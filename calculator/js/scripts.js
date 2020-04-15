61b73d369dba148996a54bad69a5616e8eaf504d
vinay prabhushankar <zephyr.vinay@gmail.com>
2020-04-14 20:52:18 -0700

    adding different calculator function

---
+++ calculator/js/scripts.js
@@ -0,0 +1,21 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1,number2){
+  return number1 - number2;
+};
+
+var multiply = function(number1,number2){
+  return number1 * number2; 
+};
+
+var divide = function(number1,number2){
+  return number1/number2; 
+};
+
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = divide(number1, number2);
+alert(result);
