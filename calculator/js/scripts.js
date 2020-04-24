6fdd4306b84d7a520340f98f13c8ae53b0b86213
vinay prabhushankar <zephyr.vinay@gmail.com>
2020-04-23 19:45:38 -0700

    adding the output div

--- calculator/js/scripts.js
+++ calculator/js/scripts.js
@@ -14,8 +14,13 @@ var divide = function(number1,number2){
   return number1/number2; 
 };
 
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-alert(result);
+// Everything below this line is user interface (or front-end) logic:
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
